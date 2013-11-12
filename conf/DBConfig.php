8e245bad093d9c00dbffe00dd91dbfce6a72b797
pprmint <neclarin@stratpoint.com>
2013-11-12 23:30:40 +0800

    added migration

--- conf/DBConfig.php
+++ conf/DBConfig.php
@@ -4,7 +4,7 @@
 	$db_config['host'] 			= "localhost";
 	$db_config['name'] 			= "rboard";
 	$db_config['username'] 		= "root";
-	$db_config['password'] 		= "root";
+	$db_config['password'] 		= "pepermint";
 	$db_config['driver'] 		= "mysqli";
 	
 
