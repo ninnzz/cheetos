1e976872029c9bf7cb0483485b4e3a2400a36ef5
Kevin Ventura <kventura@stratpoint.com>
2013-11-13 02:36:54 +0800

    click to show new updates

--- rboard/js/script.js
+++ rboard/js/script.js
@@ -1,6 +1,10 @@
 $( function () {
 
-	var prepend = [];
+	var id_list = [];
+	var fresh_count = 0;
+	var feed_cache = "";
+
+	/* START THE FETCH */
 
 	$.ajax( {
 		type: "GET",
@@ -11,18 +15,13 @@ $( function () {
 
 		_.each( result.data.result, function(d) {
 
-			prepend.push(d.id);
+			id_list.push(d.id);
 
 			html += '<div class="post msg col-lg-12" data-id=' + d.id + '>';
-			
 				html += '<p>' + unescape(decodeURIComponent(d.message)) + '</p> ';
-
 				html += '<p><span class="label label-default timestamp" data-time=' + d.date_created + '>' + d.date_created + '</span></p>';
-
 				html += ' <p>' + unescape(decodeURIComponent(d.place_tag)) + ", " + unescape(decodeURIComponent(d.sender)) + ", " + d.sender_number +'</p>';
-
 				html += '<div class="fb-share-button" data-href="http://www.reliefboard.com/m/' + d.id + '" data-type="button_count"></div>';
-
         	html += '</div>';
 
 		});
@@ -31,16 +30,18 @@ $( function () {
         $( ".timestamp" ).prettyDate();
 
 	});
+
+	/* PRETTY DATE */
 	
 	setInterval( function() {
-	
-		$( ".timestamp" ).prettyDate();
+
+		$( ".timestamp" ).prettyDate();    
     
     }, 10000);
 
-    setInterval( function() {
+	/* FETCH  */
 
-    	console.log(prepend);
+    setInterval( function() {
 
     	$.ajax( {
 			type: "GET",
@@ -51,32 +52,43 @@ $( function () {
 
 			_.each( result.data.result, function(d) {
 
-			  	if(  ! ( $.inArray( d.id , prepend ) > -1 ) ) {
+			  	if(  ! ( $.inArray( d.id , id_list ) > -1 ) ) {
 
-			  	prepend.push(d.id);
+			  		id_list.push(d.id);
 
-				html += '<div class="post msg col-lg-12" data-id=' + d.id + '>';
-				
-					html += '<p>' + decodeURIComponent(d.message) + '</p> ';
+					html += '<div class="post msg col-lg-12" data-id=' + d.id + '>';
+						html += '<p>' + decodeURIComponent(d.message) + '</p> ';
+						html += '<p><span class="label label-default timestamp" data-time=' + d.date_created + '>' + d.date_created + '</span></p>';
+						html += ' <p>' + decodeURIComponent(d.place_tag) + ", " + decodeURIComponent(d.sender) + ", " + d.sender_number +'</p>';
+						html += '<div class="fb-share-button" data-href="http://www.reliefboard.com/m/' + d.id + '" data-type="button_count"></div>';
+		        	html += '</div>';
 
-					html += '<p><span class="label label-default timestamp" data-time=' + d.date_created + '>' + d.date_created + '</span></p>';
+		        	feed_cache = html + feed_cache;
 
-					html += ' <p>' + decodeURIComponent(d.place_tag) + ", " + decodeURIComponent(d.sender) + ", " + d.sender_number +'</p>';
-
-					html += '<div class="fb-share-button" data-href="http://www.reliefboard.com/m/' + d.id + '" data-type="button_count"></div>';
-
-	        	html += '</div>';
+		        	fresh_count = fresh_count + 1;
 
 				}
 
 			});
 
-	        $( "#msg" ).prepend( html );
-	        $( ".timestamp" ).prettyDate();
+			if( fresh_count > 0 ) {
+				$(".notif").show();
+				$(".notif").css("display","block");
+				$("#count").text(fresh_count);
+			}
 
 		});
 
-    }, 5000); 
+    }, 5000);
+
+	$(document).on("click", ".notif", function(e) {
+		e.preventDefault();
+	    $( "#msg" ).prepend( feed_cache );
+	    $(".notif").hide();
+	    $( ".timestamp" ).prettyDate();
+	    feed_cache = "";
+	    fresh_count = 0;
+	});
 
     $( ".timestamp" ).prettyDate();
 
