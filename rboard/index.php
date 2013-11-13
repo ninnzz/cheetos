d26dba4db8674d4f38a55e8ddde48ceab7ae5075
Kevin Ventura <kventura@stratpoint.com>
2013-11-13 11:08:58 +0800

    js fix

--- rboard/index.php
+++ rboard/index.php
@@ -2,14 +2,23 @@
 <html lang="en">
   <head>
 
+    <!-- META -->
+    
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- META COPY FOR SEO -->
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>ReliefBoard</title>
+
+    <!-- GOOGLE ANALYTICS -->
+    
+    
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/index.css" rel="stylesheet">
+
+    <title>ReliefBoard</title>
 
   </head>
 
--- rboard/post.php
+++ rboard/post.php
@@ -18,6 +18,7 @@
   <body>
 
   <div id="fb-root"></div>
+
   <script>(function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
@@ -25,6 +26,8 @@
     js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=214855112027480";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));</script>
+
+  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
     <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
@@ -49,10 +52,17 @@
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
+      
       <div class="container">
         <br />
+        
         <p>THE POST GOES HERE. WAITING FOR THE API</p>
+        
+        <div class="fb-like" data-href="http://reliefboard.com<?php echo $_SERVER["REQUEST_URI"]; ?>" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+        <div><a href="https://twitter.com/share"  data-text="<INSERT MESSAGE HERE> #reliefboard VIA reliefboard.com" class="twitter-share-button" data-lang="en" data-related="reliefboardph:The official account of ReliefBoard">Tweet</a></div>
+
       </div>
+
     </div>
 
     <div class="container">
