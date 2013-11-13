e9229e9764684f08f17dee274b0f8788695ff475
pprmint <neclarin@stratpoint.com>
2013-11-13 12:56:10 +0800

    fm

--- db_drivers/mysqli.php
+++ db_drivers/mysqli.php
@@ -196,10 +196,7 @@
 			$link->autocommit(FALSE);
 
 			$query_message = "INSERT into {$table} values({$data});";
-
-
-		print_r($query_message);
-		print_r('-------------------');
+			
 			if(!$result = $link->query($query_message)){
 				$err = $link->error;
 				$errNo = $link->errno;
