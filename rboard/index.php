26316434a7c7db9a0ada948ba4f0ba2cdd60629e
Kevin Ventura <kventura@stratpoint.com>
2013-11-14 21:22:25 +0800

    removed duplicate facebook sdk call

--- rboard/index.php
+++ rboard/index.php
@@ -60,16 +60,6 @@
       } (document, 'script', 'facebook-jssdk'));
     </script>
 
-    <div id="fb-root"></div>
-    <script>(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=214855112027480";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-    </script>
-
     <!-- END - SOCIAL NETWORK SCRIPTS -->
 
     <!-- START - FIXED NAV -->
