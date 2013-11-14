410c0394f5b762982ed190b6fae8fd99220e53c7
Kevin Ventura <kventura@stratpoint.com>
2013-11-14 22:04:02 +0800

    meta fix

--- rboard/index.php
+++ rboard/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml">
 
-  <head prefix="og: http://ogp.me/ns/website#">
+  <head prefix="og: http://ogp.me/ns#">
 
     <title>ReliefBoard</title>
 
