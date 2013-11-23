7d7bf5200bba19296271e282eebb2c39ade236a4
pprmint <neclarin@stratpoint.com>
2013-11-23 11:33:20 +0800

    fix stuff

--- ph/apidoc.php
+++ ph/apidoc.php
@@ -72,8 +72,9 @@
 				<tr>
 					<td>Params</td>
 					<td>
-						(int)offset<br />
-						(int)limit
+						(int) offset<br />
+						(int) limit
+						<br/>(string) parent_id - post_id of parent message, gets all the comments/responses for a post with id "parent_id"
 					</td>
 				</tr>
 				<tr>
@@ -84,18 +85,25 @@
   "status": "Success",
   "data": {
 	"result": [
-	  {
-		"id": "bd4e6b11ff9ef39a45651d9f91ac3ba4",  post_id  
-		"place_tag": "manila",                     place or location of sender
-		"sender": "Z-clothing%2520Line",           name of sender        
-		"sender_number": "",                       sender number
-		"message": "",                  
-		"date_created": "1384587346",
-		"date_updated": "1384587346",
-		"fb_post_link": null,   
-		"status": "pending",                      (pending,flagged,approved)
-		"source": "web.primary"                   (web.primary,globe.mobile,smart.mobile)
-	  }
+	{
+	 "id": "92e472fc949f2db20a3a415a9a02e57e",		post_id
+	 "parent_id": null,					parent_id if the message is a comment
+	 "place_tag": "lapaz%20leyte",				place/location associated with message
+	 "sender": "people%20of%20la%20paz",			name/organization of sender
+	 "sender_number": "",					sender number if from sms
+	 "message": "they%20have%20not%20recieve%20any%20food%20and%20water.",
+	 "date_created": "1385174265",
+	 "date_updated": "1385174265",
+	 "status": "pending",					(pending,flagged,approved)
+	 "source": "sms.semaphore",				(web.primary,sms.semaphore,sms.smart,sms.globe) or external APP source
+	 "source_type": null,
+	 "action_status": "pending",				('pending','in_progress','done','expired') status of request
+	 "fb_id": "",						fb_id associated with poster/sender
+	 "tags": "",						tags for this message
+	 "expiry_date": null,
+	 "logo": null,						logo for external APP (bangonPH, goHelpPH, Rappler)
+	 "app_name": null					external app name
+	}
 	],
 	"result_count": 1,
   },
@@ -133,16 +141,23 @@
   "data": {
 	"result": [
 	{
-	  "id": "bd4e6b11ff9ef39a45651d9f91ac3ba4",    post_id  
-	  "place_tag": "manila",                       place or location of sender
-	  "sender": "Z-clothing%2520Line",             name of sender        
-	  "sender_number": "",                         sender number
-	  "message": "",                  
-	  "date_created": "1384587346",
-	  "date_updated": "1384587346",
-	  "fb_post_link": null,   
-	  "status": "pending",                        (pending,flagged,approved)
-	  "source": "web.primary"                     (web.primary,globe.mobile,smart.mobile)
+	 "id": "92e472fc949f2db20a3a415a9a02e57e",		post_id
+	 "parent_id": null,					parent_id if the message is a comment
+	 "place_tag": "lapaz%20leyte",				place/location associated with message
+	 "sender": "people%20of%20la%20paz",			name/organization of sender
+	 "sender_number": "",					sender number if from sms
+	 "message": "they%20have%20not%20recieve%20any%20food%20and%20water.",
+	 "date_created": "1385174265",
+	 "date_updated": "1385174265",
+	 "status": "pending",					(pending,flagged,approved)
+	 "source": "sms.semaphore",				(web.primary,sms.semaphore,sms.smart,sms.globe) or external APP source
+	 "source_type": null,
+	 "action_status": "pending",				('pending','in_progress','done','expired') status of request
+	 "fb_id": "",						fb_id associated with poster/sender
+	 "tags": "",						tags for this message
+	 "expiry_date": null,
+	 "logo": null,						logo for external APP (bangonPH, goHelpPH, Rappler)
+	 "app_name": null					external app name
 	}
 	],
 	"result_count": 1,
@@ -157,6 +172,74 @@
 				</tr>
 			</table>
 
+
+
+			<h3>Get all the comments/response for a post</h3>
+			<table>
+				<tr>
+					<td>Endpoint</td>
+					<td>http://reliefboard.com/comments/</td>
+				</tr>
+				<tr>
+					<td>Method</td>
+					<td>GET</td>
+				</tr>
+				<tr>
+					<td>Params</td>
+					<td>
+						parent_id (string) id of the parent post<br/>
+						offset (int)<br/>
+						limit (int)
+					</td>
+				</tr>
+				<tr>
+					<td>Result</td>
+					<td>
+						<pre>
+{
+  "status": "Success",
+  "data": {
+	"result": [
+	{
+		"id": "aaecc1822cdcc50e6a3a7bc34d1f767e",
+		"parent_id": "76bbbcdb775d8051cd9a0fb64ba07b59",
+		"place_tag": null,
+		"sender": "StarsTrak%2520Dev",
+		"sender_number": "",
+		"message": "this%2520is%2520a%2520test.",
+		"date_created": "1384851340",
+		"date_updated": "1384851340",
+		"status": "pending",
+		"source": "2b198w.reliefboard.web",
+		"source_type": null,
+		"action_status": "pending",
+		"fb_id": null,
+		"tags": null,
+		"expiry_date": null,
+		"logo": "http://rboard.me/rboard/img/profile-pic-205.jpg",
+		"app_name": "Relief Board Website"
+	}
+	],
+	"result_count": 1,
+  },
+  "method": "GET",
+  "memory_usage": "1.02MB",
+  "elapsed_time": 0.0027821063995361,
+  "compress_output": true
+}
+						</pre>
+					</td>
+				</tr>
+			</table>
+
+
+
+
+
+
+
+
+
 			<h3>Post message</h3>
 			<table>
 				<tr>
@@ -168,11 +251,24 @@
 					<td>POST</td>
 				</tr>
 				<tr>
+					<td>Notes</td>
+					<td>You need an APP_ID to access this API. Contact <em style='color:red;font-size:14px;'>nreclarin@gmail.com/neclarin@stratpoint.com</em> for APP_ID <br/>You post will automatically be posted to fb and twitter account of ReliefBoard</td>
+				</tr>
+				<tr>
 					<td>Params</td>
 					<td>
-						app_id<br />
+						<em style='color:red;'>Required</em><br/>
+						app_id <br />
 						message (text)<br />
 						name (varchar(32))
+						<br />
+						<em style='color:red;'>Optional</em><br/>
+						address (text) location associated with post <br/>
+						user_number (text) number associated with post <br/>
+						fb_id (string) fb_id associated with post/poster <br/>
+						fb_id (string) fb_id associated with post/poster <br/>
+						tags (string) tags associated with the post for easy searching <br/>
+						parent_id (string) if the post is a response/comment to another post, put the parent id of the parent post
 					</td>
 				</tr>
 				<tr>
@@ -204,10 +300,10 @@
 				<tr>
 					<td>Params</td>
 					<td>
-						loc (varchar(32))<br />
-						name (varchar(32))<br />
-						message (text)<br />
-						query <br />
+						loc - set to 1 to enable location search<br />
+						name - set to 1 to enable name search<br />
+						message - set to 1 to enable message search<br />
+						query - the search string <br />
 						offset (int)<br />
 						limit (int)
 					</td>
