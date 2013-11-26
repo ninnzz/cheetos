e97f74eef785a0f95258dbe86921f35043ed2719
pprmint <neclarin@stratpoint.com>
2013-11-26 10:18:58 +0800

    fm

--- application/model/tag_model.php
+++ application/model/tag_model.php
@@ -11,6 +11,12 @@ class Tag_model extends Kiel_Model{
 	{
 		return $this->data_handler->get_where('messages',array('tags'),array('id'=>$id),null,null,null,'date_created','');
 	}
+
+	public function search_tag($tag,$offset=0,$limit=0)
+	{
+		$query = "SELECT * FROM messages where tags like '%{$tag}%' ORDER BY date_created desc LIMIT {$offset}, {$limit};";
+		return $this->data_handler->query($query);
+	}
 }
 
 ?>
